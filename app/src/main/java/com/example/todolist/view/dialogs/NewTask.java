18d2066a9b68af547c835a7891c84115a97fa9ad
Lior Shor <liorshor123@gmail.com>
2021-04-21 10:57:04 +0300

    Subtask is now available

--- app/src/main/java/com/example/todolist/view/dialogs/NewTask.java
+++ app/src/main/java/com/example/todolist/view/dialogs/NewTask.java
@@ -1,23 +1,24 @@
 package com.example.todolist.view.dialogs;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.util.AttributeSet;
-import android.util.DisplayMetrics;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.example.todolist.R;
 import com.example.todolist.model.Task;
-import com.example.todolist.model.TaskList;
+import com.example.todolist.model.TaskMap;
 import com.example.todolist.view.adapters.TaskAdapter;
 import com.example.todolist.view.fragments.SubTaskFragment;
 import com.example.todolist.view.fragments.TaskFragment;
@@ -25,35 +26,76 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
 
-public class NewTask extends ConstraintLayout implements TaskAdapter.ItemCallBack {
-    private Dialog m_NewTaskDialog;
-    private TextView m_TitleTextView;
-    private TextView m_FriendEmail;
-    private CheckBox m_AddFriendCheckBox;
+
+public class NewTask extends TaskFragment implements TaskAdapter.ItemCallBack {
+    private final Dialog m_NewTaskDialog;
+    private final TextView m_TitleTextView;
+    private final TextView m_FriendEmail;
+    private final CheckBox m_AddFriendCheckBox;
+    private final ImageView m_TaskImage;
     private boolean isUserExist = false;
-    private FragmentManager fragmentManager;
+    private final FragmentManager fragmentManager;
     private static final FirebaseAuth m_Auth = FirebaseAuth.getInstance();
     private static final String TAG = "NewTask";
     private final SimpleDateFormat dateFormatForDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
 
     public NewTask(@NonNull Context context, FragmentManager fragmentManager) {
-        super(context);
         m_NewTaskDialog = new Dialog(context);
         setDialogSettings();
         m_TitleTextView = m_NewTaskDialog.findViewById(R.id.editTextTitle);
         m_FriendEmail = m_NewTaskDialog.findViewById(R.id.editTextTextFriendEmail);
         m_AddFriendCheckBox = m_NewTaskDialog.findViewById(R.id.addFriendCheckBox);
+        m_TaskImage = m_NewTaskDialog.findViewById(R.id.uploadImage);
         setOnClickAddFriendCheckBox();
         setOnClickCreateNewTaskButton();
+        setOnClickImage();
         this.fragmentManager = fragmentManager;
     }
+
+    private void setOnClickImage() {
+        m_TaskImage.setOnClickListener(view -> openGallery());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==0 && resultCode == Activity.RESULT_OK){
+            try {
+                Bundle bundle = data.getExtras();
+                Bitmap bitmap = bundle.getParcelable("data");
+                m_TaskImage.setImageBitmap(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void openGallery(){
+        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // ******** code for crop image
+        i.putExtra("crop", "true");
+        i.putExtra("aspectX", 100);
+        i.putExtra("aspectY", 100);
+        i.putExtra("outputX", 256);
+        i.putExtra("outputY", 356);
+
+        try {
+
+            i.putExtra("return-data", true);
+            Objects.requireNonNull(getActivity()).startActivityForResult(Intent.createChooser(i, "Select Picture"), 0);
+
+        }catch (ActivityNotFoundException ex){
+            ex.printStackTrace();
+        }
+    }
+
     private void setOnClickAddFriendCheckBox(){
         m_AddFriendCheckBox.setOnCheckedChangeListener((compoundButton, isChecked) -> m_FriendEmail.setEnabled(isChecked));
     }
@@ -64,43 +106,31 @@ public class NewTask extends ConstraintLayout implements TaskAdapter.ItemCallBac
         {
             if(titleAndEmailValidation()) {
                 String dateCreated = dateFormatForDate.format(Calendar.getInstance().getTimeInMillis());
-                Task task = new Task(m_TitleTextView.getText().toString(),dateCreated, "1");
-                List<Task> newTask = TaskList.getInstance().getTaskList();
-                newTask.add(task);
-                writeNewTaskToDB();
+                Task task = new Task(m_TitleTextView.getText().toString(),dateCreated, UUID.randomUUID().toString());
+                Map<String,Task> newTask = TaskMap.getInstance().getTaskMap();
+                newTask.put(task.getID(),task);
+                writeNewTaskToDB(task);
                 Log.d(TAG, "setOnClickCreateNewTaskButton: " + task);
                 m_NewTaskDialog.dismiss();
                 moveToSubTaskFragment();
             }
         });
     }
+
     private void moveToSubTaskFragment(){
         FragmentManager fragmentManager = this.fragmentManager;
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.flContent, new SubTaskFragment()).addToBackStack(null).commit();
     }
-    private void writeNewTaskToDB(){
+    private void writeNewTaskToDB(Task task){
         FirebaseUser firebaseUser = m_Auth.getCurrentUser();
         assert firebaseUser != null;
         String uid = firebaseUser.getUid();
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference databaseReference = database.getReference("tasks").child(uid);
-        databaseReference.setValue(TaskList.getInstance().getTaskList());
-
-/*        databaseReference.child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Log.d(TAG, "onDataChange: "+snapshot);
-                User user = snapshot.getValue(User.class);
-                user.getTasks().add(task);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });*/
+        DatabaseReference databaseReference = database.getReference("tasks").child(uid).child(task.getID());
+        databaseReference.setValue(task);
     }
+
     private boolean titleAndEmailValidation(){
         boolean validationSuccess = true;
         if(m_TitleTextView.getText().toString().isEmpty()) {
@@ -138,27 +168,11 @@ public class NewTask extends ConstraintLayout implements TaskAdapter.ItemCallBac
         return isUserExist;
     }
 
-    public NewTask(@NonNull Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public NewTask(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
     private void setDialogSettings() {
         m_NewTaskDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
         m_NewTaskDialog.setContentView(R.layout.dialog_new_task);
         m_NewTaskDialog.show();
         m_NewTaskDialog.setCanceledOnTouchOutside(true);
-        setDialogWidthAndHeight();
-
-    }
-    private void setDialogWidthAndHeight() {
-        DisplayMetrics metrics = getResources().getDisplayMetrics();
-        int m_Width = metrics.widthPixels;
-        int m_Height = metrics.heightPixels;
-        m_NewTaskDialog.getWindow().setLayout((6 * m_Width)/7, (4 * m_Height)/5);
     }
 
     @Override
--- app/src/main/java/com/example/todolist/view/fragments/TaskFragment.java
+++ app/src/main/java/com/example/todolist/view/fragments/TaskFragment.java
@@ -1,13 +1,18 @@
 package com.example.todolist.view.fragments;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import com.example.todolist.R;
 import com.example.todolist.view.adapters.SwipeToDeleteCallback;
 import com.example.todolist.view.adapters.TaskAdapter;
@@ -18,8 +23,27 @@ public class TaskFragment extends Fragment implements TaskAdapter.ItemCallBack {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private TaskAdapter taskAdapter;
+    DataPassListener mCallback;
+    public interface DataPassListener{
+        void passData(int data);
+    }
     public TaskFragment() {
         // Required empty public constructor
+    }
+    @Override
+    public void onAttach(@NonNull Context context)
+    {
+        super.onAttach(context);
+        // This makes sure that the host activity has implemented the callback interface
+        // If not, it throws an exception
+        try
+        {
+            mCallback = (DataPassListener) context;
+        }
+        catch (ClassCastException e)
+        {
+            throw new ClassCastException(context.toString()+ " must implement OnImageClickListener");
+        }
     }
 
     @Override
@@ -44,7 +68,31 @@ public class TaskFragment extends Fragment implements TaskAdapter.ItemCallBack {
         taskAdapter = new TaskAdapter();
         RecyclerView recyclerView = view.findViewById(R.id.taskList);
         setUpRecyclerView(recyclerView);
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getContext(), recyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                        mCallback.passData(position);
+                        Toast.makeText(getActivity(), "HEhe", Toast.LENGTH_SHORT).show();
+                    }
 
+                    @Override public void onLongItemClick(View view, int position) {
+                        // do whatever
+                    }
+                })
+        );
+        SearchView searchView = view.findViewById(R.id.taskSearch);
+        searchView.setOnClickListener(v -> searchView.setIconified(false));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String text) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String text) {
+                taskAdapter.getFilter().filter(text);
+                return true;
+            }
+        });
         return view;
     }
     private void setUpRecyclerView(RecyclerView recyclerView) {
@@ -54,7 +102,10 @@ public class TaskFragment extends Fragment implements TaskAdapter.ItemCallBack {
                 ItemTouchHelper(new SwipeToDeleteCallback(taskAdapter,getContext()));
         itemTouchHelper.attachToRecyclerView(recyclerView);
     }
-
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public void updateList() {
